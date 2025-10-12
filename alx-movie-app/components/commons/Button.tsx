2a002cc2e802cbf303e33e9fe5582de8a10e3173
mohamed keytsane <osamakeysane20@gmail.com>
2025-10-12 23:42:45 +0300

    .

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import { ButtonProps } from "@/interfaces";
 
-export default function Button() {
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-    <button className="px-4 py-2 bg-blue-500 text-white rounded">
-      Click Me
+    <button
+      onClick={action}
+      className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300"
+    >
+      {title}
     </button>
   );
-}
+};
+
+export default Button;
--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,17 +1,15 @@
-import React, { ReactNode } from "react";
+import { ComponentProps } from "@/interfaces";
 import Header from "./Header";
 import Footer from "./Footer";
 
-interface LayoutProps {
-  children: ReactNode;
-}
-
-export default function Layout({ children }: LayoutProps) {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <>
       <Header />
-      <main className="flex-grow container mx-auto p-4">{children}</main>
+      <main className="min-h-screen">{children}</main>
       <Footer />
-    </div>
+    </>
   );
-}
+};
+
+export default Layout;
